00f3172cc23467d6e1a1299b013a546e2aa262f5
MUHAMMEDHASHIMCP <cpmohdhashim@gmail.com>
2023-02-16 22:55:00 +0530

    unused variables removed

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -3,7 +3,6 @@ import ReactStars from "react-rating-stars-component";
 import { Link, useLocation } from "react-router-dom";
 import prodcompare from "../images/prodcompare.svg";
 import wish from "../images/wish.svg";
-import wishlist from "../images/wishlist.svg";
 import watch from "../images/watch.jpg";
 import watch2 from "../images/watch-1.avif";
 import addcart from "../images/add-cart.svg";
